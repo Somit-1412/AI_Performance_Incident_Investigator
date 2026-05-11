6ce22ac7d2665bb59c8f05e1826f9aade79bfd45
Somit Karadbhajane <somitkaradbhajane95@gmail.com>
2026-05-11 18:48:42 +0530

    Integrate Prometheus application monitoring

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
 import time
 import random
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter(
+    "app_requests_total",
+    "Total App HTTP Requests",
+    ["method", "endpoint"]
+)
 
 @app.route("/fast")
 def fast():
+    REQUEST_COUNT.labels(method="GET", endpoint="/fast").inc()
     return {
         "status": "success",
         "message": "Fast endpoint response"
@@ -15,6 +22,7 @@ def fast():
 
 @app.route("/slow")
 def slow():
+    REQUEST_COUNT.labels(method="GET", endpoint="/slow").inc()
     time.sleep(3)
 
     return {
@@ -25,6 +33,7 @@ def slow():
 
 @app.route("/error")
 def error():
+    REQUEST_COUNT.labels(method="GET", endpoint="/error").inc()
     return {
         "status": "failure",
         "message": "Simulated server error"
@@ -33,7 +42,7 @@ def error():
 
 @app.route("/cpu")
 def cpu():
-
+    REQUEST_COUNT.labels(method="GET", endpoint="/cpu").inc()
     total = 0
 
     for i in range(10000000):
@@ -47,10 +56,14 @@ def cpu():
 
 @app.route("/")
 def home():
+    REQUEST_COUNT.labels(method="GET", endpoint="/").inc()
     return {
         "message": "AI Performance Incident Investigator Backend Running"
     }
 
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
